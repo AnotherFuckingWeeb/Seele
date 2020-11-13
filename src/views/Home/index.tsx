69a17c0db15a1b2cc15b436cfe4b336be3ae11ad
Yeferson <yefersonsmarter@gmail.com>
2020-11-13 02:35:58 -0430

    first commit

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,7 +6,7 @@ import { NavBar } from '../../components/NavBar'
 import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
-    
+
     render() : JSX.Element {
         return(
             <main>
